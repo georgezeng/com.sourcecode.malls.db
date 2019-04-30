c3cae8c27ee9cd6f06e16443e774422b35d13f80
georgezeng <zzeng@bglcorp.com.au>
2019-04-30 14:01:56 +0800

    no message

---
+++ src/main/resources/db/v15.sql
@@ -0,0 +1,38 @@
+--liquibase formatted sql
+
+--changeset admin:15
+
+CREATE TABLE `goods_item` (
+	`id` bigint(20) NOT NULL AUTO_INCREMENT,
+	`merchant_id` bigint(20) NOT NULL,
+	`category_id` bigint(20) NOT NULL,
+	`brand_id` bigint(20) NOT NULL,
+	`name` varchar(50) NOT NULL,
+	`code` varchar(50),
+	`market_price` decimal(20,2),
+	`selling_points` varchar(100),
+	`content` text,
+	`thumbnail` varchar(255),
+	`enabled` bit(1) NOT NULL,
+	`create_by` varchar(50) NOT NULL,
+	`update_by` varchar(50) NOT NULL,
+	`create_time` datetime NOT NULL,
+	`update_time` datetime NOT NULL,
+	PRIMARY KEY (`id`),
+	INDEX `idx_Goods_Item_merchant_id` (`merchant_id`),
+	INDEX `idx_Goods_Item_name` (`name`)
+);
+
+
+CREATE TABLE `goods_item_photo` (
+	`id` bigint(20) NOT NULL AUTO_INCREMENT,
+	`item_id` bigint(20) NOT NULL,
+	`path` varchar(255) NOT NULL,
+	`order_num` int(11) NOT NULL,
+	`create_by` varchar(50) NOT NULL,
+	`update_by` varchar(50) NOT NULL,
+	`create_time` datetime NOT NULL,
+	`update_time` datetime NOT NULL,
+	PRIMARY KEY (`id`),
+	INDEX `idx_Goods_Item_Photo_item_id` (`item_id`)
+);
